a33e6c16ee2d02339db51502c0b4a79220aaf554
ehsansid21 <ehsansid6357@gmail.com>
2026-04-04 01:15:08 +0530

    feat: initial spring boot setup with working API

---
+++ src/main/java/transaction/example/transmansys/TransmansysApplication.java
@@ -0,0 +1,13 @@
+package transaction.example.transmansys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransmansysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransmansysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/transaction/example/transmansys/TransmansysApplicationTests.java
@@ -0,0 +1,13 @@
+package transaction.example.transmansys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransmansysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
